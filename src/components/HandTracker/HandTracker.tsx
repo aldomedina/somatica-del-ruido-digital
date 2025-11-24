5f7093d095a525dae7ce7af2c25a0e509757224f
Fran Ibarra <faibarra1@uc.cl>
2025-11-24 12:57:08 -0300

    feat: add webcam

---
+++ src/components/HandTracker/HandTracker.tsx
@@ -0,0 +1,52 @@
+import { useState, useRef } from "react";
+
+import css from "./HandTracker.module.css";
+import WebcamViewer from "./WebcamViewer/WebcamViewer";
+import CanvasRender from "./WebcamCanvasRender/WebcamCanvasRender";
+
+type pointerPositionType = {
+  x: number;
+  y: number;
+};
+
+type handTrackerType = {
+  pinchDistance: number;
+  setPinchDistance: (value: number) => void;
+};
+
+function HandTracker({ pinchDistance, setPinchDistance }: handTrackerType) {
+  const [webcamOn, setWebcamOn] = useState<boolean>(false);
+  const [pointerPosition, setPointerPosition] = useState<pointerPositionType>({
+    x: 0,
+    y: 0,
+  });
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  const mediapipeCamRef = useRef(null);
+
+  return (
+    <div className={css.container}>
+      <WebcamViewer
+        webcamOn={webcamOn}
+        setWebcamOn={setWebcamOn}
+        webcamRef={webcamRef}
+        mediapipeCamRef={mediapipeCamRef}
+      />
+      {webcamOn && (
+        <CanvasRender
+          webcamRef={webcamRef}
+          canvasRef={canvasRef}
+          mediapipeCamRef={mediapipeCamRef}
+          setPinchDistance={setPinchDistance}
+          setPointerPosition={setPointerPosition}
+        />
+      )}
+      <div>Pinch Level: {pinchDistance}</div>
+      <div>
+        Pointer Position: ({pointerPosition.x}, {pointerPosition.y})
+      </div>
+    </div>
+  );
+}
+
+export default HandTracker;
--- src/pages/ScenePage.tsx
+++ src/pages/ScenePage.tsx
@@ -1,24 +1,31 @@
+import { useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { useControls } from "leva";
 import Scene3D from "../three/Scene3D";
+import HandTracker from "../components/HandTracker/HandTracker";
 
 function ScenePage() {
-  const { collapse } = useControls({
-    collapse: {
-      value: 0.5,
-      min: 0.06,
-      max: 1,
-      step: 0.01,
-      label: "Collapse Amount",
-    },
-  });
+  const [pinchDistance, setPinchDistance] = useState<number>(0);
+  // const { collapse } = useControls({
+  //   collapse: {
+  //     value: 0.5,
+  //     min: 0.06,
+  //     max: 1,
+  //     step: 0.01,
+  //     label: "Collapse Amount",
+  //   },
+  // });
 
   return (
-    <div style={{ height: "100%" }}>
+    <div style={{ height: "100%", display: "flex", gap: "10px" }}>
+      <HandTracker
+        pinchDistance={pinchDistance}
+        setPinchDistance={setPinchDistance}
+      />
       <Canvas camera={{ position: [0, 0, 10], fov: 50 }}>
         <OrbitControls />
-        <Scene3D collapse={collapse} />
+        <Scene3D collapse={pinchDistance * 0.2} />
       </Canvas>
     </div>
   );
