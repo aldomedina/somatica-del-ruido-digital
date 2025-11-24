10a00b67395cdee38dfec0bb32de76b9e374d194
aldomedina <aldo.medina.z@gmail.com>
2025-11-24 09:24:24 -0300

    image db

---
+++ src/pages/ScenePage.tsx
@@ -0,0 +1,27 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { useControls } from "leva";
+import Scene3D from "../three/Scene3D";
+
+function ScenePage() {
+  const { collapse } = useControls({
+    collapse: {
+      value: 0.5,
+      min: 0.06,
+      max: 1,
+      step: 0.01,
+      label: "Collapse Amount",
+    },
+  });
+
+  return (
+    <div style={{ height: "100%" }}>
+      <Canvas camera={{ position: [0, 0, 10], fov: 50 }}>
+        <OrbitControls />
+        <Scene3D collapse={collapse} />
+      </Canvas>
+    </div>
+  );
+}
+
+export default ScenePage;
